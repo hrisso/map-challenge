433f7ef4978412e0246214d092cc84f72c69c1d4
hrisso <heather.risso@gmail.com>
2021-03-31 19:43:31 -0400

    map renders

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import { useEffect, useState } from 'react';
+import mapboxgl from 'mapbox-gl';
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiaHJpc3NvIiwiYSI6ImNrbXR1eHQ1aDB2NGgydW14dGZrMTM0YW4ifQ.m-RNG_OwqXr2RlUZ1WE5rQ';
 
 function App() {
+  const [lng, setLng] = useState(-115.590876702138573);
+  const [lat, setLat] = useState(32.676567128293193);
+
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: 'map', // container ID
+      style: 'mapbox://styles/mapbox/streets-v11', // style URL
+      center: [-115.590876702138573, 32.676567128293193], // starting position [lng, lat]
+      zoom: 9 // starting zoom
+    });
+    
+    const fetchLngLat = async () => {
+      try {
+        const resp = await axios.get('http://localhost:3001/api/v1/solar_farms/1/technicians');
+        console.log(resp)
+        resp.data.features.map((feature) =>
+          new mapboxgl.Marker().setLngLat(feature.geometry.coordinates).addTo(map)
+        )
+      } catch (error) {
+        throw error
+      }
+    }
+    fetchLngLat()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id='map'></div>
     </div>
   );
 }
