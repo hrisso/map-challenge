87d992aaff827d0dd358f7a51921ec377c961f85
hrisso <heather.risso@gmail.com>
2021-04-02 14:58:25 -0400

    notifications attempt

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,9 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiaHJpc3NvIiwiYSI6ImNrbXR1eHQ1aDB2NGgydW14dGZrM
 
 function App() {
   const [locations, setLocations] = useState([]);
+  const [techLocA, setTechLocA] = useState({})
+  const [techLocB, setTechLocB] = useState({})
+  const [techLocC, setTechLocC] = useState({})
 
   useEffect(() => {
     const map = new mapboxgl.Map({
@@ -15,6 +18,9 @@ function App() {
       center: [-115.590, 32.676], // starting position [lng, lat]
       zoom: 14 // starting zoom
     });
+
+    // Grab data with axios GET request, map over response to create marker for each technician
+    // Push makers to array to cleanup later
 
     let currentMarkers = [];
     
@@ -31,6 +37,8 @@ function App() {
         throw error
       }
     }
+    
+    // Clear old markers using marker array and clear out marker array once removed
 
     const clearMarkers = () => {
       if (currentMarkers.length > 0) {
@@ -41,16 +49,39 @@ function App() {
       }
     }
 
+    // Notification when technicians are less than 304.8m from each other
+    
+    const notify = () => {
+      setTechLocA(new mapboxgl.LngLat(locations[0].geometry.coordinates[0], locations[0].geometry.coordinates[1]));
+      setTechLocB(new mapboxgl.LngLat(locations[1].geometry.coordinates[0], locations[1].geometry.coordinates[1]));
+      setTechLocC(new mapboxgl.LngLat(locations[2].geometry.coordinates[0], locations[2].geometry.coordinates[1]));
+    
+      console.log(locations)
+      console.log(techLocA.distanceTo(techLocB))
+      console.log(techLocB.distanceTo(techLocC))
+      console.log(techLocA.distanceTo(techLocC))
+      
+      if (techLocA.distanceTo(techLocB) < 304.8 || techLocB.distanceTo(techLocC) < 304.8 || techLocA.distanceTo(techLocC) < 304.8) {
+        let popup = new mapboxgl.Popup({ closeOnClick: false })
+        .setLngLat([-115.590, 32.676])
+        .setHTML('<h1>Technicians are close to each other!</h1>')
+        .addTo(map);
+      }
+    }
+    
+    // Fetch locations on mount
+
     fetchLocations()
+    
+    // Ping for new data every minute, remove old markers, add new ones, and check for notifications
 
     const interval = setInterval(() => {
       clearMarkers()
       fetchLocations()
+      notify()
     },10000)
 
     return()=>clearInterval(interval)
-
-//window.alert(name)
 
   }, [])
 
